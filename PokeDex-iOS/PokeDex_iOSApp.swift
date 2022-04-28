75027e01f16176ee5324bfc721f136dc21402d33
Kemal Maulana <kemalmaulana19@gmail.com>
2022-04-28 19:20:06 +0700

    Initial Commit

---
+++ PokeDex-iOS/PokeDex_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeDex_iOSApp.swift
+//  PokeDex-iOS
+//
+//  Created by Kemal Maulana on 28/04/22.
+//
+
+import SwiftUI
+
+@main
+struct PokeDex_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
